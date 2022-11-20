fdcdcda2dd532bb43c60308548996b49f5f6d0ab
AmberG08 <118612002+AmberG08@users.noreply.github.com>
2022-11-20 00:46:45 +0000

    Add files via upload
    To do
    
    - when you click "Admit Defeat" button, the popup is shown
    - on the popup, when you click yes, the tab closes
    - when you click no, you continue the game

---
+++ dynamics.js
@@ -0,0 +1,27 @@
+const starting_seconds = 10;
+let time = starting_seconds;
+
+const countdownEl = document.getElementById('countdown');
+
+setInterval(update, 1000);
+
+function update(){
+    const seconds = time;
+    //let remainder = time / 10;
+
+    if (seconds>0) {
+
+    countdownEl.innerHTML = seconds + ':00';
+    time--;
+
+    } else {
+        countdownEl.innerHTML = '00:00';
+    }
+}
+
+const defeat = document.getElementById('defeat');
+
+defeat.addEventListener('click', () => {
+    popup.classList.add('show');
+})
+
